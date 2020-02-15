1a61d55567b91c3b3eaf555beb81754fdf59de96
fhrancoo <132franco@gmail.com>
2020-02-15 01:57:38 -0300

    Primer Commit

---
+++ js/eventos.js
@@ -0,0 +1,6 @@
+Swal.fire({
+    title: "ATENCIÓN!",
+    icon: "warning",
+    text: "Recomendamos usar Chroome como navegador para la mejor experiencia del usuario.",
+    backdrop: true,
+});
---
+++ js/slaider.js
@@ -0,0 +1,65 @@
+//Set the slideIndex to 1 i.e 1st image
+var slideIndex = 1;
+
+//showImage() to display the 1st image
+showImage(slideIndex);
+
+/*When the user clicks one of the buttons call plusIndex(). The plusIndex() function subtracts one or adds one to the slideIndex*/
+function plusIndex(n) {
+    showImage(slideIndex += n);
+}
+function showImage(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    
+/*If the slideIndex is higher than the number of elements (x.length), the slideIndex is set to zero*/
+    if (n > x.length) {
+    slideIndex = 1
+    }
+    
+/*If the slideIndex is less than 1 it is set to number of elements (x.length) */
+    if (n < 1) {
+    slideIndex = x.length
+    } ;  
+    for (i = 0; i < x.length; i++) {
+    
+/*The showImage() function hides (display="none" ) all elements with the class name "slides"*/
+    x[i].style.display = "none";
+    }  
+/*displays (display="block" ) the element with the given slideIndex*/
+    x[slideIndex-1].style.display = "block";
+}
+
+//AutoSlide animation
+var slideIndex = 0;
+autoSlide();
+function autoSlide() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {
+    slideIndex = 1
+    }
+    x[slideIndex-1].style.display = "block";
+
+// Change image every 5 seconds
+setTimeout(autoSlide, 5000);
+}
+
+// Muestra botones cuando paso el mouse encima de otro div
+
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+function botones() {
+    prev.style.display = "flex";
+    next.style.display = "flex";
+}
+
+function botones2() {
+    prev.style.display = "none";
+    next.style.display = "none";
+}
